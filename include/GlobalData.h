73140aab5a9c53639dcdc9f0b43ae4bfcae8465a
Tao Wang <twang@bigpoint.net>
2019-09-28 17:27:06 +0200

    code organize

--- include/GlobalData.h
+++ include/GlobalData.h
@@ -1,5 +1,10 @@
 #pragma once
 
+/*
+use static to store global data that never can be changed after the tree build finished.
+this way can speed up the recursive function deal with stacking and unstacking instead of pass them as a parameter.
+*/
+
 #include <vector>
 
 class KdLeaf;
@@ -7,7 +12,10 @@ struct Point;
 class GlobalData
 {
 public:
+	// the invalid leaf that has no elements.
 	static KdLeaf* TrivialLeaf;
+	// all original input points.
 	static std::vector<Point> OriginalPoints;
+	// indicate the point index that used for easier sort when building the tree.
 	static int* SortedIndices;
 };
